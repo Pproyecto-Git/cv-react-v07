3b007f4bbc55bd04cd1ae2ced3a43c00ce1db985
Gillson Andrey Martinez <gilanmarsa@gmail.com>
2025-12-02 19:04:17 -0500

    actualizacion del archivo perfil.jsx

--- src/components/Perfil.jsx
+++ src/components/Perfil.jsx
@@ -1,20 +1,3 @@
-import React from "react";
-
-export default function CabeceraCV({ nombre, cargo, ciudad, contacto }) {
-  const { email, telefono } = contacto || {};
-  return (
-    <header>
-      <h1>{nombre}</h1>
-      <h2>{cargo}</h2>
-      <p>
-        {ciudad} {email && <>• {email}</>} {telefono && <>• {telefono}</>}
-      </p>
-      <hr />
-    </header>
-  );
-}
-
-// Perfil.jsx
 import React from 'react'
 
 export default function Perfil({ resumen }) {
